9792e11adb03002b141d8208dc2806167cfb22a1
Putu Denanta Bayuguna Perteka <putubayuguna@gmail.com>
2024-09-04 22:47:26 +0800

    update

--- src/views/coming_soon.tsx
+++ src/views/coming_soon.tsx
@@ -44,7 +44,7 @@ const ComingSoonPage = () => {
       <img
         src={bg_barber}
         className="w-full h-screen object-cover fixed"
-        style={{ opacity: 0.06 }}
+        style={{ opacity: 0.02 }}
       />
 
       <div className="w-full flex justify-center pt-32">
@@ -99,14 +99,19 @@ const ComingSoonPage = () => {
         </div>
       </div>
 
-      <div className="w-full h-[100px] xl:h-[1228.32px] absolute top-[70%]">
-        <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="2500">
-          <div className="w-[360px] md:w-[400px] xl:w-[440px] h-[469.73px] left-1/2 -translate-x-1/2 top-[-0px] absolute bg-[#ff1818] rounded-full blur-[276.57px]" />
-          <div className="w-[380px] md:w-[800px] xl:w-[1006.67px] xl:h-[1228.32px] left-1/2 -translate-x-1/2 top-[-0px] absolute  rounded-[17.84px] ">
-            <img
-              className="left-2 xl:left-10 top-0 absolute "
-              src={bg_dashboard}
-            />
+      <div className="relative mt-10">
+        <div className="w-full h-[100px] xl:h-[1228.32px] absolute top-0">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            data-aos-delay="2500">
+            <div className="w-[360px] md:w-[400px] xl:w-[440px] h-[469.73px] left-1/2 -translate-x-1/2 top-[-0px] absolute bg-[#ff1818] rounded-full blur-[276.57px]" />
+            <div className="w-[380px] md:w-[800px] xl:w-[1006.67px] xl:h-[1228.32px] left-1/2 -translate-x-1/2 top-[-0px] absolute  rounded-[17.84px] ">
+              <img
+                className="left-2 xl:left-10 top-0 absolute "
+                src={bg_dashboard}
+              />
+            </div>
           </div>
         </div>
       </div>
